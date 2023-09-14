377a5e47f6d876f105360c4a1006d1baa337d9f9
TabathaLarissa <tabatha.alves@icloud.com>
2023-09-14 12:39:10 -0300

    Update versão

--- page_transp.py
+++ page_transp.py
@@ -1,39 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import os
-# import base64
-
-# def get_file(bin_file, file_label='File'):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     bin_str = base64.b64encode(data).decode()
-#     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_label}">Download {file_label} File</a>'
-#     return href
-
-# def transpose_excel():
-#     st.title("Transpor Arquivo Excel")
-    
-#     uploaded_file = st.file_uploader("Arraste seu arquivo Excel ou clique para selecionar", type=['xls', 'xlsx'])
-
-#     if uploaded_file:
-#         df = pd.read_excel(uploaded_file, engine='openpyxl')
-#         st.write('Prévia do arquivo:')
-#         st.dataframe(df.head())
-
-#         if st.button('Transpor'):
-#             df = df.transpose()
-#             st.write('Prévia do arquivo transposto:')
-#             st.dataframe(df.head())
-
-#             file_name = "transposed_file.xlsx"
-#             df.to_excel(file_name, index=True)
-
-#             st.success('Arquivo Transposto com Sucesso!')
-#             st.markdown(get_file(file_name, file_label=file_name), unsafe_allow_html=True)
-            
-#             # Apague o arquivo após disponibilizá-lo para download
-#             os.remove(file_name)
-
 import streamlit as st
 import pandas as pd
 import os
